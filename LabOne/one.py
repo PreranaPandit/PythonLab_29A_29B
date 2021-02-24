e719fd5ffdad250c6f7206ceb229f068d9bfd48d
PreranaPandit <preranapandit07@gmail.com>
2021-02-24 14:15:45 +0545

    Initial commit

---
+++ LabOne/one.py
@@ -0,0 +1,9 @@
+#1.write a program that takes three numbers and prints their sum.
+# Every number is given on separate lines.
+
+
+num1 = int(input("enter the first value: "))
+num2 = int(input("enter the second value: "))
+num3 = int(input("enter the third value: "))
+sum = num1 + num2 + num3
+print(f"The sum of given three numbers is {sum}")
