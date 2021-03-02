c704f44b58a910c94fa73205974e59c5364780ca
PreranaPandit <preranapandit07@gmail.com>
2021-03-02 14:33:05 +0545

    sum of first n  positive integers

--- LabOne/five.py
+++ LabOne/five.py
@@ -1,9 +1,12 @@
 '''
 5. A school decided to replace the desks in three classrooms. Each desk sits two students.
-Given the number of students in each class, print the smallest possible number of desks that can be purchased.
-The program should read three integers: the number of students in each of the three classes, a, b and c respectively.
+Given the number of students in each class, print the smallest possible number of desks
+that can be purchased.
+The program should read three integers: the number of students in each of the three classes,
+a, b and c respectively.
  In the first test there are three groups. The first group has 20 students and thus needs 10 desks.
-  The second group has 21 students, so they can get by with no fewer than 11 desks. 11 desks are also enough
+  The second group has 21 students, so they can get by with no fewer than 11 desks.
+   11 desks are also enough
   for the third group of 22 students. So, we need 32 desks in total.
 '''
 
@@ -24,6 +27,5 @@ remain_class2 = (no_student_class2 % 2)
 print(f"Remaining desk for second class is {remain_class2} ")
 remain_class3 = (no_student_class3 % 2)
 print(f"remaining desk for third class is {remain_class3} ")
-
 total_desk = desk_class1 + desk_class2 + desk_class3 + remain_class1 + remain_class2 + remain_class3
 print(f"Total number of desks that can be purchased is {total_desk} ")
---
+++ LabOne/nine.py
@@ -0,0 +1,20 @@
+'''
+ 9. Write a python program to find sum of the first n positive integers.
+sum = (n*(n+1))/2
+'''
+
+##########################################################################
+
+# n = int(input("Input a number: "))
+# sum_num = (n * (n + 1)) / 2
+# print(sum_num)
+
+##########################################################################
+
+list=[1,2,3,4,5,6,7,8,9,10]
+total=sum(list)
+print(f"the sum of given positive integer is {total} ")
+
+
+
+
