bb59a1b9e352fb92ef8e04c875b7b679ef02a559
PreranaPandit <preranapandit07@gmail.com>
2021-04-05 09:49:46 +0545

    operators exercise

---
+++ LabTwo/qfifteen.py
@@ -0,0 +1,14 @@
+'''
+15. What will be the output of the following if a =2, b=3, c =4, d = 3?
+a) a == b
+b) a != d
+c) b == d
+d) a != c
+e) a += c
+f) b /= d
+g) b > a
+h) a < d
+i) b-a == c-b
+j) b >= d
+'''
+
